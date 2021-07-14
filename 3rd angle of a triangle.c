4c36a7c0839742a8ba2c47f54cb1c18b97c4b1c3
Charita-20wh1a1292 <85917271+Charita-20wh1a1292@users.noreply.github.com>
2021-07-14 10:45:26 +0530

    Add files via upload

---
+++ 3rd angle of a triangle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter any two angles of a triangle\n");
+	scanf("%d%d",&a,&b);
+	c = 180-(a+b);
+	printf("third angle = %d",c);
+	return 0;
+}
