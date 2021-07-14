b75c91783537b12136c5f006ab7aeec2790bc3d3
Charita-20wh1a1292 <85917271+Charita-20wh1a1292@users.noreply.github.com>
2021-07-14 16:23:01 +0530

    Add files via upload

---
+++ simple if eligible to vote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter age of person\n");
+	scanf("%d",&a);
+	if(a>=18)
+	printf("person whose age is %d is eligible to vote",a);
+	return 0;
+}
+
+
+
