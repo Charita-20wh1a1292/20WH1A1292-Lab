603109da0243cb141c7e19c01aa1fbf0e68acfba
Charita-20wh1a1292 <85917271+Charita-20wh1a1292@users.noreply.github.com>
2021-07-14 16:28:56 +0530

    Add files via upload

---
+++ largest of two numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a=8,b=8;
+	if(a>b)
+	printf("a is the largest number");
+	else if(a<b)
+	printf("b is the largest number");
+	else
+	printf("a is equal to b");
+	return 0;
+}
