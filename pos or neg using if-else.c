0aca43b19e3d9e26945b9f3b3a7c62cd6d2add05
Charita-20wh1a1292 <85917271+Charita-20wh1a1292@users.noreply.github.com>
2021-07-14 16:28:23 +0530

    Add files via upload

---
+++ pos or neg using if-else.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("Enter a number\n");
+	scanf("%d",&a);
+	if(a>0)
+	printf("%d is positive",a);
+	else
+	printf("%d is negative",a);
+	return 0;
+}
