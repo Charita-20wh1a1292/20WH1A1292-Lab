2706dba61c66f874d6331580a38a6441ba61bca0
Charita-20wh1a1292 <85917271+Charita-20wh1a1292@users.noreply.github.com>
2021-07-14 17:05:20 +0530

    Add files via upload

---
+++ do while alp.c
@@ -0,0 +1,16 @@
+#include<stdio.h>  
+int main()  
+{  
+    char ch = 'a';  
+  
+    printf("lowecase English Alphabets:\n"); 
+	do
+	 {  
+        printf("%c ", ch);  
+        ch++;  
+    }  
+    while(ch <= 'z');  
+    return 0;  
+}  
+
+
