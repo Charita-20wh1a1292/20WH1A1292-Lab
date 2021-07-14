aa65cddf5df6cdc845994c38c8d3e12b5d4d66de
Charita-20wh1a1292 <85917271+Charita-20wh1a1292@users.noreply.github.com>
2021-07-14 10:18:22 +0530

    Add files via upload

---
+++ reversing char.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	char x,y,z;
+		printf("enter three characters\n");
+		scanf("%c%c%c",&x,&y,&z);
+		printf("After reversing\n");
+		printf("%c%c%c",z,y,x);
+		return 0;
+}
