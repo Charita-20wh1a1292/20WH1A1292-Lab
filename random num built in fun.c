19199b8c1b9a87d1628fa3e1060575693b0e6f38
Charita-20wh1a1292 <85917271+Charita-20wh1a1292@users.noreply.github.com>
2021-08-06 19:19:59 +0530

    Add files via upload

---
+++ random num built in fun.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{  
+	printf("the random number is %d\n",rand());
+    printf("the random number is %d\n",rand());
+	printf("the random number is %d\n",rand());
+	return 0;
+}
