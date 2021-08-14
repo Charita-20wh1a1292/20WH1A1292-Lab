e541a32f52ef96ab4b536b08ea0e46ae47891813
Charita-20wh1a1292 <85917271+Charita-20wh1a1292@users.noreply.github.com>
2021-08-14 19:20:46 +0530

    Add files via upload

---
+++ transpose_matrix_pointers.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int *matrix,i,j,r,c;
+	printf("Enter no.of rows:\n");
+	scanf("%d",&r);
+	printf("Enter no.of columns:\n");
+	scanf("%d",&c);
+	matrix=(int*)calloc(r*c,sizeof(int));
+	printf("Enter elements:\n");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			scanf("%d",matrix+(i*c+j)*sizeof(int));
+		}
+	}
+	printf("The matrix is:\n");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			printf("%d\t",*(matrix+(i*c+j)*sizeof(int)));
+		}
+		printf("\n");
+	}
+	printf("Transpose of the matrix is:\n");
+	for(i=0;i<c;i++)
+	{
+		for(j=0;j<r;j++)
+		{
+			printf("%d\t",*(matrix+(j*c+i)*sizeof(int)));
+		}
+		printf("\n");
+	}
+}
+	
+	
+	
