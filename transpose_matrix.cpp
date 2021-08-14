2db3d671a7c198de4bd0504a856c88ec9d76b7f5
Charita-20wh1a1292 <85917271+Charita-20wh1a1292@users.noreply.github.com>
2021-08-14 18:55:53 +0530

    Add files via upload

---
+++ transpose_matrix.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+	int a[3][3],b[3][3],i,j;
+	printf("Enter matrix elements:\n");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	printf("Matrix is:\n");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("%d\t",a[i][j]);
+		}
+		printf("\n");
+	}
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			b[j][i]=a[i][j];
+		}
+	}
+	printf("Transpose of matrix is:\n");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("%d\t",b[i][j]);
+		}
+		printf("\n");
+	}
+}
