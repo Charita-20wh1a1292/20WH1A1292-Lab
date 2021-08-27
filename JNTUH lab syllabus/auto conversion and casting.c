1bfd2561b0ac691c94d53761ffbb1bc12f3db90b
Charita-20wh1a1292 <85917271+Charita-20wh1a1292@users.noreply.github.com>
2021-08-27 07:18:13 +0530

    Add files via upload

---
+++ JNTUH lab syllabus/auto conversion and casting.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int sum = 17,count = 5;
+	double mean;
+	mean = (double)sum/count;
+	printf("value of mean is %f\n",mean);
+	int i = 23,sum1;
+	char c = 'c';
+	sum1 = i + 'c';
+	printf("value of sum1 is %d\n",sum1);
+	
+}
